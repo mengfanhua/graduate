e2d97ab54bdaa6e0ab6d2e019902269b98293be9
mengfanhua <1070985939@qq.com>
2021-01-26 22:10:57 +0800

    对比式UI结构

--- ImageSolve/modeAdjustWidget/twoShowWidget.py
+++ ImageSolve/modeAdjustWidget/twoShowWidget.py
@@ -1,4 +1,6 @@
 from ImageSolve.config import *
+from ImageSolve.secondWidget.mixWidget import MixWidget
+from ImageSolve.secondWidget.bottomWidget import BottomWidget
 
 
 class TwoShowWidget(QWidget):
@@ -7,3 +9,15 @@ class TwoShowWidget(QWidget):
                  lastPage, nextPage, imageFrontBox, imageBackBox,
                  exchangeShowMode, imagePaste):
         super(TwoShowWidget, self).__init__()
+        self.layout = QVBoxLayout()
+        self.aboveWidget = MixWidget(openFrontImage, sizeFrontAdjust,
+                                     angleFrontAdjust, imageFrontBox)
+        self.middleWidget = MixWidget(openBackImage, sizeBackAdjust, angleBackAdjust, imageBackBox)
+        self.bottomWidget = BottomWidget(lastPage, nextPage, exchangeShowMode, imagePaste)
+        self.layout.addWidget(self.aboveWidget)
+        self.layout.addWidget(self.middleWidget)
+        self.layout.addWidget(self.bottomWidget)
+        self.layout.setStretch(0, 4)
+        self.layout.setStretch(1, 4)
+        self.layout.setStretch(2, 1)
+        self.setLayout(self.layout)
