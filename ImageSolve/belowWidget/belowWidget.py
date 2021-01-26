7970e06634e2a7037db8cec6cda8ffb18f9f8649
mengfanhua <1070985939@qq.com>
2021-01-26 21:49:07 +0800

    修改了下部分widget的结构

--- ImageSolve/belowWidget/belowWidget.py
+++ ImageSolve/belowWidget/belowWidget.py
@@ -1,15 +1,11 @@
 from ImageSolve.config import *
-from ImageSolve.belowWidget.imageBox import ImageBox
 
 
 class BelowWidget(QWidget):
-    def __init__(self):
+    def __init__(self, frontImage, backImage):
         super(BelowWidget, self).__init__()
-        self.frontImage = ImageBox()
-        self.backImage = ImageBox()
         self.layout = QStackedLayout()
         self.layout.setStackingMode(QStackedLayout.StackAll)
-        self.layout.addWidget(self.frontImage)
-        self.layout.addWidget(self.backImage)
+        self.layout.addWidget(frontImage)
+        self.layout.addWidget(backImage)
         self.setLayout(self.layout)
-        self.backImage.flag = -1
