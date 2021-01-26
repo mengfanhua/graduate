b42850bd5db9ab9de709bbca3b421be9d15f50a5
mengfanhua <1070985939@qq.com>
2021-01-26 21:43:41 +0800

    修改了按钮结构，拓展使用

--- ImageSolve/aboveWidget/buttonWidget/openImageWidget.py
+++ ImageSolve/aboveWidget/buttonWidget/openImageWidget.py
@@ -2,11 +2,9 @@ from ImageSolve.config import *
 
 
 class OpenImageWidget(QWidget):
-    def __init__(self):
+    def __init__(self, frontImage, backImage):
         super(OpenImageWidget, self).__init__()
-        self.frontImage = QPushButton("选择前景图")
-        self.backImage = QPushButton("选择后景图")
         layout = QVBoxLayout()
-        layout.addWidget(self.frontImage)
-        layout.addWidget(self.backImage)
+        layout.addWidget(frontImage)
+        layout.addWidget(backImage)
         self.setLayout(layout)
