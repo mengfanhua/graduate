e3c3a8a5f2f7b271ef93e1e4379ee61ff8c3a172
mengfanhua <1070985939@qq.com>
2021-01-26 21:47:52 +0800

    修改了参数部分

--- ImageSolve/aboveWidget/doubleAdjustWidget.py
+++ ImageSolve/aboveWidget/doubleAdjustWidget.py
@@ -3,15 +3,12 @@ from ImageSolve.aboveWidget.adjustWidget.adjust import AdjustWidget
 
 
 class DoubleAdjustWidget(QWidget):
-    def __init__(self):
+    def __init__(self, frontAdjust, backAdjust):
         super(DoubleAdjustWidget, self).__init__()
-        self.frontAdjust = AdjustWidget()
-        self.backAdjust = AdjustWidget()
         self.layout = QStackedLayout()
         self.layout.setStackingMode(QStackedLayout.StackOne)
-        self.frontAdjust.setWindowFlags(Qt.WindowStaysOnTopHint)
-        self.backAdjust.setWindowFlags(Qt.WindowStaysOnBottomHint)
-        self.layout.addWidget(self.frontAdjust)
-        self.layout.addWidget(self.backAdjust)
+        frontAdjust.setWindowFlags(Qt.WindowStaysOnTopHint)
+        backAdjust.setWindowFlags(Qt.WindowStaysOnBottomHint)
+        self.layout.addWidget(frontAdjust)
+        self.layout.addWidget(backAdjust)
         self.setLayout(self.layout)
-
