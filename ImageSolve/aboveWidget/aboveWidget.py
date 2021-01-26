921276db040ab59a00b0470ab16b4b63db3487a9
mengfanhua <1070985939@qq.com>
2021-01-26 21:47:08 +0800

    修改了aboveWidget的实现结构

--- ImageSolve/aboveWidget/aboveWidget.py
+++ ImageSolve/aboveWidget/aboveWidget.py
@@ -4,14 +4,21 @@ from ImageSolve.aboveWidget.buttonWidget.openImageWidget import OpenImageWidget
 
 
 class AboveWidget(QWidget):
-    def __init__(self):
+    def __init__(self,openFrontImage, openBackImage, sizeFrontAdjust,
+                 sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                 lastPage, nextPage, exchangeShowMode, imagePaste, exchangeFrontBack):
         super(AboveWidget, self).__init__()
-        self.openImage = OpenImageWidget()
-        self.adjust = DoubleAdjustWidget()
-        self.exchange = QPushButton("front")
-        layout = QHBoxLayout()
-        layout.addWidget(self.openImage)
-        layout.addWidget(self.adjust)
-        layout.addWidget(self.exchange)
-        layout.setAlignment(Qt.AlignLeft)
-        self.setLayout(layout)
+        self.layout = QHBoxLayout()
+        self.layout.setAlignment(Qt.AlignLeft)
+        self.openImageWidget = OpenImageWidget(openFrontImage, openBackImage)
+        self.sizeWidget = DoubleAdjustWidget(sizeFrontAdjust, sizeBackAdjust)
+        self.angleWidget = DoubleAdjustWidget(angleFrontAdjust, angleBackAdjust)
+        self.upDownWidget = OpenImageWidget(lastPage, nextPage)
+        self.functionWidget = OpenImageWidget(exchangeShowMode, imagePaste)
+        self.layout.addWidget(self.openImageWidget)
+        self.layout.addWidget(self.sizeWidget)
+        self.layout.addWidget(self.angleWidget)
+        self.layout.addWidget(self.upDownWidget)
+        self.layout.addWidget(self.functionWidget)
+        self.layout.addWidget(exchangeFrontBack)
+        self.setLayout(self.layout)
