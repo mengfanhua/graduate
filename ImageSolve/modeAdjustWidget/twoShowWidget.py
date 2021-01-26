d5ae5a6bd9a5a92ae8cf066aa8f56e02255eeece
mengfanhua <1070985939@qq.com>
2021-01-26 21:46:15 +0800

    对比式界面层，待实现

---
+++ ImageSolve/modeAdjustWidget/twoShowWidget.py
@@ -0,0 +1,9 @@
+from ImageSolve.config import *
+
+
+class TwoShowWidget(QWidget):
+    def __init__(self, openFrontImage, openBackImage, sizeFrontAdjust,
+                 sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                 lastPage, nextPage, imageFrontBox, imageBackBox,
+                 exchangeShowMode, imagePaste):
+        super(TwoShowWidget, self).__init__()
