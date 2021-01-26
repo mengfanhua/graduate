d566e9d1d20fcbd871f982e07cc6792edcbe2432
mengfanhua <1070985939@qq.com>
2021-01-26 21:45:13 +0800

    改为调用MainWindows

--- ImageSolve/run.py
+++ ImageSolve/run.py
@@ -1,9 +1,9 @@
-from ImageSolve.mainWindow import MainWindow
+from ImageSolve.mainWindows import MainWindows
 from ImageSolve.config import *
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    box = MainWindow()
+    box = MainWindows()
     box.show()
     app.exec_()
