8cd1d0309d0697f28ebd7960810badf0d7f7ec8e
mengfanhua <1070985939@qq.com>
2021-01-31 21:25:58 +0800

    添加了双击事件，但信号尚未处理

--- ImageSolve/belowWidget/imageBox.py
+++ ImageSolve/belowWidget/imageBox.py
@@ -4,6 +4,8 @@ from ImageSolve.algorithms.xyToXY import dx_to_ox
 
 
 class ImageBox(QWidget):
+    featureSignal = pyqtSignal(tuple)
+
     def __init__(self):
         super(ImageBox, self).__init__()
         self.img = None
@@ -35,13 +37,15 @@ class ImageBox(QWidget):
         if self.scaled_img:
             # 此处需更改成分块加载，避免出现显存不足越界的情况
             # 在绘图之前，需将特征点首先绘制，以用来锚定图片位置
+            # timg = ImageQt.toqpixmap(self.img)
             painter = QPainter()
             painter.begin(self)
             painter.translate(0, 0)
             painter.scale(self.scale, self.scale)
             painter.rotate(self.angle)
             # 下面绘图部分需根据具体图片大小进行分割，然后分块加载，至于起止点需通过计算得出
-            painter.drawPixmap(self.point, ImageQt.toqpixmap(self.img))
+            # painter.drawPixmap(self.point, timg)
+            # painter.drawPixmap(self.point, self.img)
             painter.end()
             # self.img.crop((left, up, right, down))
 
@@ -78,9 +82,11 @@ class ImageBox(QWidget):
 
     def mouseDoubleClickEvent(self, e):
         # 此处需要添加记录特征点模块，需要测试
-        if e.button() == Qt.LeftButton:
-            x, y = e.pos()
-            dx, dy = self.point
+        if e.button() == Qt.LeftButton and self.scaled_img == 1:
+            x, y = e.pos().x(), e.pos().y()
+            dx, dy = self.point.x(), self.point.y()
             # 此处为x', y'点，需通过反向计算得到图上对应的原点
             ox, oy = dx_to_ox(x, y, self.scale, self.angle, dx, dy)
             self.featureList.append((ox, oy))
+            self.repaint()
+            self.featureSignal.emit((ox, oy))
