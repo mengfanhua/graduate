1a532edcbe40a7cd3e7fc7f7ecc25bcea5de2b5f
mengfanhua <1070985939@qq.com>
2021-01-31 21:24:05 +0800

    添加了pyqtSignal

--- ImageSolve/config.py
+++ ImageSolve/config.py
@@ -1,4 +1,4 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog, QStackedLayout, QSlider
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog, QStackedLayout, QSlider, QScrollArea
 from PyQt5.Qt import QPixmap, QPoint, Qt, QPainter, QIcon
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, pyqtSignal
