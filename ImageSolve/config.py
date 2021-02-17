3765a49e4075c3b583edfdf843fd8cdda3230215
mengfanhua <1070985939@qq.com>
2021-02-17 16:04:04 +0800

    添加了引用库

--- ImageSolve/config.py
+++ ImageSolve/config.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog, QStackedLayout, QSlider, QScrollArea
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog, QStackedLayout, QSlider, QScrollArea, QRadioButton
 from PyQt5.Qt import QPixmap, QPoint, Qt, QPainter, QIcon, QColor, QPalette, QPen, QBrush
 from PyQt5.QtCore import QSize, pyqtSignal, QThread
 import os
