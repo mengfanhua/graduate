a16c59a41996b0dd3a2e11909018f693f49907a7
mengfanhua <1070985939@qq.com>
2021-02-17 16:04:52 +0800

    test

--- test.py
+++ test.py
@@ -7,12 +7,19 @@ class widget(QWidget):
         super(widget, self).__init__()
         self.asd = PointWidget((1, 2, 3), 1)
         self.zxc = PointWidget((1, 5, 3), 2)
+        self.radioButton = QRadioButton("click")
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.asd)
         self.layout.addWidget(self.zxc)
+        self.layout.addWidget(self.radioButton)
         self.setLayout(self.layout)
         self.asd.deletePoint.connect(self.delWidget)
         self.zxc.deletePoint.connect(self.delWidget)
+        self.radioButton.clicked.connect(self.tett)
+
+    def tett(self):
+        print(self.radioButton.isChecked())
+        self.radioButton.setChecked(False)
 
     # 测试layout对组件的删除作用
     def delWidget(self, value):
