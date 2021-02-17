0ef5cfcb3c747c082e8ced1ac5846b453edc57b5
mengfanhua <1070985939@qq.com>
2021-02-17 15:25:47 +0800

    添加了加载瓦片的按钮选项

--- ImageSolve/aboveWidget/buttonWidget/openImageWidget.py
+++ ImageSolve/aboveWidget/buttonWidget/openImageWidget.py
@@ -2,9 +2,11 @@ from ImageSolve.config import *
 
 
 class OpenImageWidget(QWidget):
-    def __init__(self, frontImage, backImage):
+    def __init__(self, frontImage, backImage, radio=None):
         super(OpenImageWidget, self).__init__()
         layout = QVBoxLayout()
         layout.addWidget(frontImage)
         layout.addWidget(backImage)
+        if radio is not None:
+            layout.addWidget(radio)
         self.setLayout(layout)
