d8568a617ba5406045c9dc2be3a8cfbab592190e
mengfanhua <1070985939@qq.com>
2021-02-27 12:58:53 +0800

    颜色生成，16颜色循环使用

--- ImageSolve/algorithms/colorGenerate.py
+++ ImageSolve/algorithms/colorGenerate.py
@@ -1,36 +1,19 @@
 import numpy as np
 import math
 
+colorList = [(136, 0, 21), (237, 28, 36), (255, 127, 39), (255, 242, 0), (34, 177, 76),
+             (0, 162, 232), (63, 72, 204), (163, 73, 164), (185, 122, 87),
+             (255, 174, 201), (255, 201, 14), (239, 228, 176), (181, 230, 29),
+             (153, 217, 234), (112, 146, 190), (200, 191, 231)]
+
 
 def ColorGenerate(value):
     """
-    通过颜色图的扩展，获得黑白色与颜色图的立体图，通过螺旋方法获得颜色点
     :param value: the number of point * 240 / 45
     :return:color: calculated color
     """
-    x = round(value * math.sin(value) / 2) + 120
-    y = round(value * math.cos(value) / 4) + 60
-    print(x, y)
-    max_sum = 255 - round(y * 128 / 240.0)
-    min_sum = round(y * 127 / 240.0)
-    if x < 40:
-        te = np.array([max_sum, min_sum + round(x * (max_sum - min_sum) / 40.0), min_sum])
-    elif x < 80:
-        te = np.array([max_sum - round((x-40) * (max_sum - min_sum) / 40.0), max_sum, min_sum])
-    elif x < 120:
-        te = np.array([min_sum, max_sum, min_sum + round((x - 80) * (max_sum - min_sum) / 40.0)])
-    elif x < 160:
-        te = np.array([min_sum, max_sum - round((x - 120) * (max_sum - min_sum) / 40.0), max_sum])
-    elif x < 200:
-        te = np.array([min_sum + round((x - 160) * (max_sum - min_sum) / 40.0), min_sum, max_sum])
-    else:
-        te = np.array([max_sum, min_sum, max_sum - round((x - 200) * (max_sum - min_sum) / 40.0)])
-    if value < 120:
-        temp = np.array([255, 255, 255])
-        color = (temp - te) * (120 - value) / 120 + te
-    else:
-        color = te - te * (value - 120) / 120
-    return color
+    global colorList
+    return colorList[(value - 1) % len(colorList)]
 
 
 def ColorTranslate(color):
