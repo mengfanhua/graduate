c51c203e70bb50adc215a2582ab09c4e08fbc2ff
mengfanhua <1070985939@qq.com>
2021-03-04 20:04:23 +0800

    修改了第二界面的结构

--- ImageSolve/modeAdjustWidget/twoShowWidget.py
+++ ImageSolve/modeAdjustWidget/twoShowWidget.py
@@ -1,23 +1,54 @@
 from ImageSolve.config import *
-from ImageSolve.secondWidget.mixWidget import MixWidget
-from ImageSolve.secondWidget.bottomWidget import BottomWidget
+from ImageSolve.secondWidget.hBoxShowWidget import HBoxShowWidget
+from ImageSolve.secondWidget.vBoxShowWidget import VBoxShowWidget
 
 
 class TwoShowWidget(QWidget):
     def __init__(self, openFrontImage, openBackImage, sizeFrontAdjust,
                  sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
                  lastPage, nextPage, imageFrontBox, imageBackBox,
-                 exchangeShowMode, imagePaste, point1, point2, radio):
+                 exchangeShowMode, imagePaste, point1, point2, radio, changeLayout):
         super(TwoShowWidget, self).__init__()
-        self.layout = QVBoxLayout()
-        self.aboveWidget = MixWidget(openFrontImage, sizeFrontAdjust,
-                                     angleFrontAdjust, imageFrontBox, point1, None)
-        self.middleWidget = MixWidget(openBackImage, sizeBackAdjust, angleBackAdjust, imageBackBox, point2, radio)
-        self.bottomWidget = BottomWidget(lastPage, nextPage, exchangeShowMode, imagePaste)
-        self.layout.addWidget(self.aboveWidget)
-        self.layout.addWidget(self.middleWidget)
-        self.layout.addWidget(self.bottomWidget)
-        self.layout.setStretch(0, 4)
-        self.layout.setStretch(1, 4)
-        self.layout.setStretch(2, 1)
+        self.layout = QStackedLayout()
+        self.layout.setStackingMode(QStackedLayout.StackOne)
+        self.value = 0
+        self.content = HBoxShowWidget(openFrontImage, openBackImage, sizeFrontAdjust,
+                                      sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                                      lastPage, nextPage, imageFrontBox, imageBackBox,
+                                      exchangeShowMode, imagePaste, point1, point2, radio, changeLayout)
+        self.layout.addWidget(self.content)
+        self.layout.setCurrentIndex(0)
         self.setLayout(self.layout)
+
+    def changeLayout(self, openFrontImage, openBackImage, sizeFrontAdjust,
+                 sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                 lastPage, nextPage, imageFrontBox, imageBackBox,
+                 exchangeShowMode, imagePaste, point1, point2, radio, changeLayout):
+        if self.value == 0:
+            self.value += 1
+            content = VBoxShowWidget(openFrontImage, openBackImage, sizeFrontAdjust,
+                                          sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                                          lastPage, nextPage, imageFrontBox, imageBackBox,
+                                          exchangeShowMode, imagePaste, point1, point2, radio, changeLayout)
+            self.layout.addWidget(content)
+            self.layout.setCurrentIndex(1)
+            self.repaint()
+        else:
+            if self.layout.currentIndex() == 1:
+                content = HBoxShowWidget(openFrontImage, openBackImage, sizeFrontAdjust,
+                                              sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                                              lastPage, nextPage, imageFrontBox, imageBackBox,
+                                              exchangeShowMode, imagePaste, point1, point2, radio, changeLayout)
+                self.layout.itemAt(0).widget().deleteLater()
+                self.layout.insertWidget(0, content)
+                self.layout.setCurrentIndex(0)
+                self.repaint()
+            else:
+                content = VBoxShowWidget(openFrontImage, openBackImage, sizeFrontAdjust,
+                                         sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                                         lastPage, nextPage, imageFrontBox, imageBackBox,
+                                         exchangeShowMode, imagePaste, point1, point2, radio, changeLayout)
+                self.layout.itemAt(1).widget().deleteLater()
+                self.layout.insertWidget(1, content)
+                self.layout.setCurrentIndex(1)
+                self.repaint()
