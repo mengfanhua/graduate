1b70b428b9e968b34b565e504a42dfa25bd7364b
mengfanhua <1070985939@qq.com>
2021-03-04 22:44:41 +0800

    上传图片到服务器，便于合成图片的展示

---
+++ ImageSolve/algorithms/updateImage.py
@@ -0,0 +1,27 @@
+import requests
+from ImageSolve.algorithms.generateKey import generate_key
+
+
+def update_image(path, z, x, y, key):
+    files = {'img': open(path, 'rb')}
+    datas = {"x": x, "y": y, "z": z, "key": key}
+    with open("./Resources/host", "r", encoding="utf-8") as f:
+        host = f.read()
+        f.close()
+    response = requests.post(host + "/update", data=datas, files=files)
+    data = response.text
+    response.close()
+    print(data)
+    if data == "true":
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    path = "C:\\Users\\meng\\Desktop\\map\\3\\0\\0.png"
+    z = "3"
+    x = "0"
+    y = "0"
+    key = generate_key("mengfanhua", "mengfanhua")
+    print(update_image(path, z, x, y, key))
