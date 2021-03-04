4043660ece0c11a99203bb6f6b11875607ea2cf6
mengfanhua <1070985939@qq.com>
2021-03-04 20:04:45 +0800

    添加了改变布局按钮

--- ImageSolve/secondWidget/bottomWidget.py
+++ ImageSolve/secondWidget/bottomWidget.py
@@ -2,12 +2,13 @@ from ImageSolve.config import *
 
 
 class BottomWidget(QWidget):
-    def __init__(self, lastPage, nextPage, exchangeShowMode, imagePaste):
+    def __init__(self, lastPage, nextPage, exchangeShowMode, imagePaste, changeLayout):
         super(BottomWidget, self).__init__()
         self.layout = QHBoxLayout()
         self.layout.addWidget(lastPage)
         self.layout.addWidget(nextPage)
         self.layout.addWidget(exchangeShowMode)
         self.layout.addWidget(imagePaste)
+        self.layout.addWidget(changeLayout)
         self.layout.setAlignment(Qt.AlignLeft)
         self.setLayout(self.layout)
