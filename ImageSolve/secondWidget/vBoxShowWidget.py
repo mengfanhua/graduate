91ff3f49a0388d047e90007b7068c308f91392b6
mengfanhua <1070985939@qq.com>
2021-03-04 20:05:53 +0800

    横向布局

---
+++ ImageSolve/secondWidget/vBoxShowWidget.py
@@ -0,0 +1,21 @@
+from ImageSolve.config import *
+from ImageSolve.secondWidget.mixWidget import MixVWidget
+from ImageSolve.secondWidget.bottomWidget import BottomWidget
+
+
+class VBoxShowWidget(QWidget):
+    def __init__(self, openFrontImage, openBackImage, sizeFrontAdjust,
+                 sizeBackAdjust, angleFrontAdjust, angleBackAdjust,
+                 lastPage, nextPage, imageFrontBox, imageBackBox,
+                 exchangeShowMode, imagePaste, point1, point2, radio, changeLayout):
+        super(VBoxShowWidget, self).__init__()
+        self.layout = QVBoxLayout()
+        self.aboveWidget = MixVWidget(openFrontImage, sizeFrontAdjust,
+                                     angleFrontAdjust, imageFrontBox, point1, radio,
+                                      openBackImage, sizeBackAdjust, angleBackAdjust, imageBackBox, point2)
+        self.bottomWidget = BottomWidget(lastPage, nextPage, exchangeShowMode, imagePaste, changeLayout)
+        self.layout.addWidget(self.aboveWidget)
+        self.layout.addWidget(self.bottomWidget)
+        self.layout.setStretch(0, 9)
+        self.layout.setStretch(1, 1)
+        self.setLayout(self.layout)
