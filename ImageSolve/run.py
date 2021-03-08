4a78aab5a3fe721567f9e8e209e3d5b9160bbdae
mengfanhua <1070985939@qq.com>
2021-03-08 18:45:51 +0800

    添加不同界面，并对不同界面进行连接

--- ImageSolve/run.py
+++ ImageSolve/run.py
@@ -1,9 +1,34 @@
 from ImageSolve.mainWindows import MainWindows
 from ImageSolve.config import *
+from ImageSolve.enterWidget.enterWidget import EnterWidget
+from ImageSolve.autoRegistrationWidget.autoRegistrationWidget import AutoRegistrationWidget
+from ImageSolve.uploadWidget.uploadWidget import UploadWidget
+from ImageSolve.combineWidget.combineWidget import CombineWidget
+from ImageSolve.executeWidget.multiImageWidget import MultiImageWidget
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    box = MainWindows()
-    box.show()
+    enter = EnterWidget()
+    auto = AutoRegistrationWidget()
+    hand = MainWindows()
+    upload = UploadWidget()
+    combine = CombineWidget()
+    oneExecute = MultiImageWidget()
+
+    enter.auto.connect(auto.show)
+    enter.hand.connect(hand.show)
+    enter.upload.connect(upload.show)
+    auto.backsignal.connect(enter.show)
+    hand.backsignal.connect(enter.show)
+    upload.backsignal.connect(enter.show)
+
+    hand.combinesignal.connect(combine.come)
+    combine.backsignal.connect(hand.show)
+
+    auto.combinesignal.connect(oneExecute.come)
+    oneExecute.backsignal.connect(auto.show)
+
+
+    enter.show()
     app.exec_()
