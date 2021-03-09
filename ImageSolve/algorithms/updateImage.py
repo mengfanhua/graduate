1179381ec8a138dc6c95a003652bb8c02044afaf
mengfanhua <1070985939@qq.com>
2021-03-09 23:08:18 +0800

    上传图片接口

--- ImageSolve/algorithms/updateImage.py
+++ ImageSolve/algorithms/updateImage.py
@@ -3,32 +3,38 @@ from ImageSolve.algorithms.generateKey import generate_key
 
 
 def update_image(path, z, x, y, key):
-    files = {'img': open(path, 'rb')}
-    datas = {"x": x, "y": y, "z": z, "key": key}
-    with open("./Resources/host", "r", encoding="utf-8") as f:
-        host = f.read()
-        f.close()
-    response = requests.post(host + "/update", data=datas, files=files)
-    data = response.text
-    response.close()
-    print(data)
-    if data == "true":
-        return True
-    else:
+    try:
+        files = {'img': open(path, 'rb')}
+        datas = {"x": x, "y": y, "z": z, "key": key}
+        with open("./Resources/host", "r", encoding="utf-8") as f:
+            host = f.read()
+            f.close()
+        response = requests.post(host + "/update", data=datas, files=files)
+        data = response.text
+        response.close()
+        print(data)
+        if data == "true":
+            return True
+        else:
+            return False
+    except:
         return False
 
 
 def validate_key(z, x, y, key):
-    datas = {"x": x, "y": y, "z": z, "key": key}
-    with open("./Resources/host", "r", encoding="utf-8") as f:
-        host = f.read()
-        f.close()
-    response = requests.post(host + "/validate", data=datas)
-    data = response.text
-    response.close()
-    if data == "true":
-        return True
-    else:
+    try:
+        datas = {"x": x, "y": y, "z": z, "key": key}
+        with open("./Resources/host", "r", encoding="utf-8") as f:
+            host = f.read()
+            f.close()
+        response = requests.post(host + "/validate", data=datas)
+        data = response.text
+        response.close()
+        if data == "true":
+            return True
+        else:
+            return False
+    except:
         return False
 
 
