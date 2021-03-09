bfc0523d773e201389f6b23aca639f3947ba32af
mengfanhua <1070985939@qq.com>
2021-03-09 18:22:27 +0800

    test

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-from ImageSolve.algorithms.generateKey import generate_key
-from ImageSolve.algorithms.updateImage import update_image
+from PIL import Image
+import cv2
+import numpy as np
 
 
 if __name__ == '__main__':
     path = "C:\\Users\\meng\\Desktop\\map\\3\\0\\0.png"
-    z = "3"
-    x = "0"
-    y = "0"
-    key = generate_key("admin", "admin")
-    print(key)
-    # print(update_image(path, z, x, y, key))
+    image = Image.open(path)
+    iamge = image.convert("L")
+    asd = cv2.cvtColor(np.asarray(iamge))
+
